9cbc05756f64751069fbe11907e36de8e9c7fe91
Tomislav Dukez <tomdu3@ymail.com>
2025-03-19 12:12:50 +0000

    add user controller

---
+++ controllers/userController.js
@@ -0,0 +1,20 @@
+const User = require('../models/User');
+
+// Get user information
+const getUserInfo = async (req, res) => {
+  try {
+    const user = await User.findById(req.userId).select('-password');
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+
+    res.status(200).json(user);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+};
+
+module.exports = {
+  getUserInfo,
+}; 
