4f9e2cd3cfc359d1019b050d9105f5d8b8759822
Tomislav Dukez <tomdu3@ymail.com>
2025-03-19 11:38:08 +0000

    Add Users schema

--- index.js
+++ index.js
@@ -1,28 +1,24 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 
 dotenv.config();
 
 const app = express();
 
 // Connect to DB
-mongoose.connect(
-    process.env.MONGODB_URI,
-    {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    })
-    .then(() => console.log('Connected to DB'))
-    .catch(err => console.log(`DB Connection Error: ${err}`));
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => console.log("Connected to DB"))
+  .catch((err) => console.log(`DB Connection Error: ${err}`));
 
-app.get('/', (req, res) => {
-    res.send('Hello World');
-}
-);
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
 
 // Start server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Server started on port ${PORT}`);
+  console.log(`Server started on port ${PORT}`);
 });
+
