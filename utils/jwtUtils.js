a25f2397b46afcfe39acab376b11b8b7169c4322
Tomislav Dukez <tomdu3@ymail.com>
2025-03-19 12:12:38 +0000

    add jwt utils middleware

---
+++ utils/jwtUtils.js
@@ -0,0 +1,24 @@
+const jwt = require('jsonwebtoken');
+
+const verifyToken = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader) {
+    return res.status(401).json({ message: 'Authorization header missing' });
+  }
+
+  const token = authHeader.split(' ')[1];
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(403).json({ message: 'Invalid token' });
+    }
+
+    req.userId = decoded.userId;
+    next();
+  });
+};
+
+module.exports = {
+  verifyToken,
+};
